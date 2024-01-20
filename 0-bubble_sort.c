e214f74906fabcfdeb7f86a66cace502989723ca
Sanaalagh <Sanaalaghzou@gmail.com>
2024-01-20 08:36:54 -0800

    Commiting the 0x1B. C - Sorting algorithms & Big O  project

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,30 @@
 #include "sort.h"
 
 /**
-  * bubble_sort - The bubble sorting function.
-  * @array: Array to sort.
-  * @size: Size of the aray.
-  * Return: Nothing.
-  */
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
-	int aux = 0;
+    if (array == NULL || size < 2)
+        return;
 
-	if (array == NULL || size == 0)
-		return;
-	for (; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				aux = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
-		}
-	}
+    size_t i, j;
+    int temp;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
 }
